63b9382e0314797fde0e797ff6f2b2bf6648468d
rossrobino <rbrobino@gmail.com>
2023-01-04 11:12:03 -0800

    fix image ratio

--- src/lib/components/about/ProfilePicture.svelte
+++ src/lib/components/about/ProfilePicture.svelte
@@ -5,5 +5,5 @@
 <img
 	src={profilePic}
 	alt="Ross in Ireland"
-	class="rounded-sm border-4 border-slate-50 shadow-sm shadow-slate-50"
+	class="rounded-sm border-4 border-slate-50 w-full aspect-[3/4]"
 />
