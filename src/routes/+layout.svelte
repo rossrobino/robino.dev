403456a7139c77fad5a6a7910ff9e60a2167119a
rossrobino <rbrobino@gmail.com>
2023-01-04 15:06:28 -0800

    add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,20 @@
 	import "../app.postcss";
 </script>
 
+<svelte:head>
+	<script
+		async
+		src="https://www.googletagmanager.com/gtag/js?id=G-VTNMKYJH7T"
+	></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag("js", new Date());
+
+		gtag("config", "G-VTNMKYJH7T");
+	</script>
+</svelte:head>
+
 <slot />
