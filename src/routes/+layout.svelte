a4408f478ffe33dc418045deb7c5fafd950632d6
rossrobino <rbrobino@gmail.com>
2023-01-04 14:50:33 -0800

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
 	import "../app.postcss";
+	import { webVitals } from "$lib/vitals";
+	import { browser } from "$app/env";
+	import { page } from "$app/stores";
+
+	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+
+	$: if (browser && analyticsId) {
+		webVitals({
+			path: $page.url.pathname,
+			params: $page.params,
+			analyticsId,
+		});
+	}
 </script>
 
 <slot />
