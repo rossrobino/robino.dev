834bd1ec8ae363ed22b125796ba3128fa965f37e
rossrobino <rbrobino@gmail.com>
2023-03-22 17:20:12 -0700

    filter out forks

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,8 +1,14 @@
 export async function load({ fetch }) {
 	const res = await fetch("https://api.github.com/users/rossrobino/repos");
-	const repos = await res.json();
-	// sort by pushed_at date, most recent first
-	repos.sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at));
+	
+	let repos = await res.json();
+
+	// sort by created_at date, most recent first
+	repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
+	// filter out forks
+	repos = repos.filter((repo) => repo.fork === false);
+
 	// creates a new array with just the data points needed for the page
 	return {
 		repos: repos.map((repo) => ({
