bc3c1ea4520572e92899240580abc38a2002b0cd
rossrobino <rbrobino@gmail.com>
2025-01-13 12:36:13 -0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="en">
-			<body className="prose">{children}</body>
+			<body className="prose">
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
